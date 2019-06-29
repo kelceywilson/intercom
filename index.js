ae94dcd6c978fd8cb33a47159b0be7b1c61c5c6c
Kelcey Wilson <kelceywilson@gmail.com>
2019-06-29 16:07:20 -0700

    Set up heroku-postbuild

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  // If route is not recognized serve index.html
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
