029745a626a53a754d4ba6f9bf9c35582f043d8e
Kelcey Wilson <kelceywilson@gmail.com>
2019-07-12 13:11:07 -0700

    Add /api/comments to serve user comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -6,6 +9,50 @@ const commentEmailTemplate = require("../services/emailTemplates/commentEmailTem
 const Comment = mongoose.model("comments");
 
 module.exports = app => {
+  app.get("/api/comments/:commentId/:choice", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  app.get("/api/comments", requireLogin, async (req, res) => {
+    const comments = await Comment.find({ _user: req.user._id }).select({
+      recipients: false
+    });
+    res.send(comments);
+  });
+
+  app.post("/api/comments/webhooks", (req, res) => {
+    console.log(req.body);
+    const p = new Path("/api/comments/:commentId/:choice");
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, commentId: match.commentId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy("email", "commentId")
+      .each(({ commentId, email, choice }) => {
+        Comment.updateOne(
+          {
+            _id: commentId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
+
   app.post("/api/comments", requireLogin, requireCredits, async (req, res) => {
     const { body, title, subject, recipients } = req.body;
     // console.log(recipients.split(",").map(email => ({ email: email.trim() })));
