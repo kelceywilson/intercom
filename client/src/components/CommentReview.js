0fed4bd4e499e868e2f776143920106348638c36
Kelcey Wilson <kelceywilson@gmail.com>
2019-07-12 13:07:46 -0700

    Add withRouter to sent history to action creator

--- client/src/components/CommentReview.js
+++ client/src/components/CommentReview.js
@@ -2,9 +2,11 @@ import _ from "lodash";
 
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import formFieldNames from "./formComponents/formFieldNames";
+import * as actions from "../actions";
 
-const CommentReview = ({ onCancel, formValues }) => {
+const CommentReview = ({ onCancel, formValues, submitComment, history }) => {
   const fields = _.map(formFieldNames, ({ name, label }) => {
     return (
       <div key={name}>
@@ -18,6 +20,13 @@ const CommentReview = ({ onCancel, formValues }) => {
       <div>CommentReview</div>
       <div>{fields}</div>
       <button onClick={onCancel}>Back</button>
+      <button
+        className="green btn-flat right"
+        onClick={() => submitComment(formValues, history)}
+      >
+        Send Comment
+        <i className="material-icons right">email</i>
+      </button>
     </div>
   );
 };
@@ -26,4 +35,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.commentForm.values };
 }
 
-export default connect(mapStateToProps)(CommentReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(CommentReview));
