0dadca4e8cf09954207c017b6918ddcf92f530ee
Kelcey Wilson <kelceywilson@gmail.com>
2019-07-12 13:16:55 -0700

    Fix typo

--- services/emailTemplates/commentEmailTemplate.js
+++ services/emailTemplates/commentEmailTemplate.js
@@ -5,15 +5,19 @@ module.exports = comment => {
     <html>
       <body>
         <div style="text-align: center;">
-        <h3>Thanks for your comment!</h3>
-        <p>Here it is:</p>
-        <p>${comment.body}</p>
-        <div>
-          <a href="${keys.redirectDomain}/api/surveys/${comment.id}/yes">Yes</a>
-        </div>
-        <div>
-          <a href="${keys.redirectDomain}/api/surveys/${comment.id}/no">No</a>
-        </div>
+          <h3>Thanks for your comment!</h3>
+          <p>Here it is:</p>
+          <p>${comment.body}</p>
+          <div>
+            <a href="${keys.redirectDomain}/api/comments/${
+    comment.id
+  }/yes">Yes</a>
+          </div>
+          <div>
+            <a href="${keys.redirectDomain}/api/comments/${
+    comment.id
+  }/no">No</a>
+          </div>
         </div>
       </body>
     </html>
