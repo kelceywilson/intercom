4bef399dbc489b796c49f68525cd71b19f90c014
Kelcey Wilson <kelceywilson@gmail.com>
2019-07-13 15:47:10 -0700

    Delete console logs

--- client/src/index.js
+++ client/src/index.js
@@ -21,11 +21,11 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-console.log(
-  "process.env.REACT_APP_STRIPE_KEY :",
-  process.env.REACT_APP_STRIPE_KEY
-);
-console.log("process.env.NODE_ENV :", process.env.NODE_ENV);
+// console.log(
+//   "process.env.REACT_APP_STRIPE_KEY :",
+//   process.env.REACT_APP_STRIPE_KEY
+// );
+// console.log("process.env.NODE_ENV :", process.env.NODE_ENV);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
